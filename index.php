627013ae204d1c7ded7392b748ef9007117ce4a7
Almuheetu Shihab <almuheetu@gmail.com>
2023-12-18 00:04:02 +0600

    do while Loop practice

--- index.php
+++ index.php
@@ -26,12 +26,22 @@
 
 
 // while loops
-$i = 1;
 
-while ($i < 100) {
-    echo $i;
-    echo "<br>";
-    $i++;
-}
+// $i = 1;
 
+// while ($i < 100) {
+//     echo $i;
+//     echo "<br>";
+//     $i++;
+// }
+
+
+
+// do while Loop
+$x = 1;
+
+do {
+  echo "The number is: $x <br>";
+  $x++;
+} while ($x <= 5);
 ?>

045627fcf6419a9e961d0e2b77218217809da2be
Almuheetu Shihab <almuheetu@gmail.com>
2023-12-17 22:02:17 +0600

    php variable types learning

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
 
 
-  echo "hello";  
-   
-  print "Shihab";
+  echo "<H1>hello</H1>";  
+  Echo "<br>";
+  Print "Shihab";
+
+
+
+
+  $x1 = "shihab";
+  $x2 = 25;
+  $is_student = Yes;
+  $height = 5.8;
+  $languages = array("Bengali", "Spanish", "English", "Hindi");
+
 
 
 ?>

093570530bc653f80726212988174b324d95dd1c
Almuheetu Shihab <almuheetu@gmail.com>
2023-12-17 22:20:27 +0600

    php string learn

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 
 
-  echo "<H1>hello</H1>";  
-  Echo "<br>";
-  Print "Shihab";
+//   echo "<H1>hello</H1>";  
+//   Echo "<br>";
+//   Print "Shihab";
 
 
 
 
   $x1 = "shihab";
   $x2 = 25;
-  $is_student = Yes;
+  $is_student = True;
   $height = 5.8;
   $languages = array("Bengali", "Spanish", "English", "Hindi");
 
-
+// echo $x1.$x2
+echo strpos($x1);
+// var_dump($height);
 
 ?>

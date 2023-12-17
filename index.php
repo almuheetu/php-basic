23c6f60ccc25988062f04909f33a8f983d36b951
Almuheetu Shihab <almuheetu@gmail.com>
2023-12-17 23:56:06 +0600

    while loops practice

--- index.php
+++ index.php
@@ -8,14 +8,30 @@
 
 
 
-  $x1 = "shihab";
-  $x2 = 25;
-  $is_student = True;
-  $height = 5.8;
-  $languages = array("Bengali", "Spanish", "English", "Hindi");
+//   $x1 = "shihab";
+//   $x2 = 25;
+//   $is_student = True;
+//   $height = 5.8;
+//   $languages = array("Bengali", "Spanish", "English", "Hindi");
 
 // echo $x1.$x2
-echo strpos($x1);
+
 // var_dump($height);
+
+
+
+
+
+// php loops
+
+
+// while loops
+$i = 1;
+
+while ($i < 100) {
+    echo $i;
+    echo "<br>";
+    $i++;
+}
 
 ?>
